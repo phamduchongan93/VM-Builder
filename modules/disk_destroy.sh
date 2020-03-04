8cc469bc8a08b165192b123ed5e667d7e76aa560
phamduchongan93 <vincentpham1993@gmail.com>
2020-03-04 03:32:37 -0500

    update disk destroy

--- modules/disk_destroy.sh
+++ modules/disk_destroy.sh
@@ -1,7 +1,8 @@
 #!/bin/bash 
+
 disk_destroy ()
 {
-  [ -n "$(virsh list --all | grep -i $1)" ] && virsh dumpxml --domain "$1" | grep 'source file' || echo ''
+  [ -n "$(virsh list --all | grep -i $1)" ] && virsh dumpxml --domain "$1" | xmlstarlet sel -t -v '//source/@file' -nl | head -1 || echo ''
 }
 
 disk_destroy "test1"

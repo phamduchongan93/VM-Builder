7a5bcf5dad45e8200dd339bd2ab1fa9780d2ee06
an pham <vincentpham1993@gmail.com>
2020-03-06 11:45:18 -0500

    update virt_install.sh

--- modules/virt_install.sh
+++ modules/virt_install.sh
@@ -9,13 +9,23 @@ ubuntu ()
   create_disk && echo 'New disk created' || echo 'No disk created'
   echo "Path is $disk_ubuntu.img"
   echo "Building the ubuntu vm ..."
-  virt-install -n "$1" --description "run in console mode" --ram=2048 --vcpu=2 --disk path="$2",bus=virtio,size="$3" --graphic none --location "$4" --extra-args="console=tty0 console=ttyS0,115200" --check all=off & 
+  virt-install -n "$1" --description "run in console mode" --ram=2048 --vcpu=2 --disk path="$2",bus=virtio,size="$3" --graphic none --location "$4" --extra-args="console=tty0 console=ttyS0,115200" --check all=off 
   ec=$? 
   [ "$ec" = 0 ] && echo "New ubuntu vm created"
 }
 
 centos ()
 {
+  # centos <vm_name> <disk_name> <disk_size> <image_location>
   echo 'Building the centos vm ...'
-  virt-install -n ubuntu --description "run in console mode" --os-type=Linux --os-variant centos7.0 --ram-2048 --vcpu=2 --disk path="",bus=virtio,size=60 --graphic none --location "/home/anpham/images/ubuntu-18.04.3-desktop-amd64.iso" --extra-args="console=tty0 console=ttyS0,115200" --check all=off 
+  virt-install -n "$1" 
+         --description "centos run in console mode" \
+         --os-type=Linux --os-variant centos7.0 \
+         --ram-2048 --vcpu=2 \
+         --disk path="2",bus=virtio,size="$3" --graphic none \
+         --location "$4" \
+         --extra-args="console=tty0 console=ttyS0,115200" \
+         --check all=off \
+  ec=$? 
+  [ "$ec" = 0 ] && echo "New centos vm created"
 }

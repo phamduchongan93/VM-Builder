653d27d7661aaadfd7cfd9ea8e421b8c65bd2c74
an pham <vincentpham1993@gmail.com>
2020-03-19 06:10:03 -0400

    add clone-vm.sh

---
+++ modules/clone-vm.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Description: clone the current running vm to a new vm with new qcow2 disk format
+
+clone-disk () {
+  # clone-disk "$source_vm" "$target_vm"
+  local source_vm="$1"
+  local target_vm="$2"
+
+  virt-clone --original "$source_vm" --name "$target_vm" -f "/var/lib/libvirt/images/$target_vm.qcow2"
+} 
